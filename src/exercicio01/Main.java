91c12225f92fbd014c3bf13451abd58bf2bff10c
Gabriel Candido <gabriel.candido.soares21@gmail.com>
2025-04-08 20:27:52 -0300

    Adicionados Exercicios 1 e 2

---
+++ src/exercicio01/Main.java
@@ -0,0 +1,29 @@
+package exercicio01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor do número A: ");
+        int a = sc.nextInt();
+        System.out.print("Digite o valor do número B: ");
+        int b = sc.nextInt();
+        System.out.print("Digite o valor do número C: ");
+        int c = sc.nextInt();
+
+        int soma = a + b;
+
+        if (soma < c) {
+            System.out.println(soma + " < " + c);
+        } else if (soma > c) {
+            System.out.println(soma + " > " + c);
+        } else {
+            System.out.println(soma + " = " + c);
+        }
+
+        sc.close();
+    }
+}
---
+++ src/exercicio02/Main.java
@@ -0,0 +1,32 @@
+package exercicio02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int n = sc.nextInt();
+
+        System.out.print("O número " + n + " é ");
+
+        if (n % 2 == 0) {
+            System.out.print("par ");
+        } else {
+            System.out.print("ímpar ");
+        }
+
+        if (n > 0) {
+            System.out.print("e positivo.");
+        } else if (n == 0) {
+            System.out.print("e neutro.");
+        } else {
+            System.out.print("e negativo.");
+        }
+
+
+        sc.close();
+    }
+}
